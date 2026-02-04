65fb533048a7b18ace6af839bb5ba7af2e28779e
Adam <adam.labruyere@gmail.com>
2026-02-04 15:24:03 +0000

    Commit Changes

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -21,5 +21,5 @@ renamed as (
 select orders_id,
         shipping_fee,
         logcost,
-        CAST(ship_cost AS INT64) AS ship_cost
+        CAST(ship_cost AS FLOAT64) AS ship_cost
         from renamed
