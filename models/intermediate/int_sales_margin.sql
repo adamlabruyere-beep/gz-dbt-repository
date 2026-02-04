f32e986b0710782144b16f5d8f11fc960dfda056
Adam <adam.labruyere@gmail.com>
2026-02-04 15:52:37 +0000

    __

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,5 +1,5 @@
 SELECT
 quantity * purchase_price AS purchase_cost,
 revenue (quantity * purchase_price) AS margin
-FROM {{ ref{stg_raw__sales.sql} }}
-FULL OUTER JOIN {{ ref{stg_raw__product.sql} }} USING(products_id)
+FROM {{ ref('sales') }}
+FULL OUTER JOIN {{ ref('product') }} USING(products_id)
