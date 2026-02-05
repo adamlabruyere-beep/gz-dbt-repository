9848b532943a5b6d9c12067c052cac9952f55adb
Adam <adam.labruyere@gmail.com>
2026-02-05 10:20:40 +0000

    logcost

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,5 +1,12 @@
-SELECT
-quantity * purchase_price AS purchase_cost,
-revenue (quantity * purchase_price) AS margin
-FROM {{ ref('sales') }}
-FULL OUTER JOIN {{ ref('product') }} USING(products_id)
+  SELECT
+      products_id,
+      date_date,
+      orders_id,
+      revenue,
+      quantity,
+      purchase_price,
+      ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
+      ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin
+  FROM {{ref("stg_raw__sales")}} s
+  LEFT JOIN {{ref("stg_raw__product")}} p
+      USING (products_id)
